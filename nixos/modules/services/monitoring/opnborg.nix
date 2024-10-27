382fd089c6d33fab98e43ef7bb55ac640102c02e
PAEPCKE, Michael <git@paepcke.de>
2024-10-27 10:58:05 +0000

    update

--- nixos/modules/services/monitoring/opnborg.nix
+++ nixos/modules/services/monitoring/opnborg.nix
@@ -9,52 +9,6 @@ with lib; let
 in {
   options.services.opnborg = {
     enable = mkEnableOption "opnborg";
-
-    apikey = mkOption {
-      type = types.str;
-      default = null;
-      example = "+RIb6YWNdcDWMMM7W5ZYDkUvP4qx6e1r7e/Lg/Uh3aBH+veuWfKc7UvEELH/lajWtNxkOaOPjWR8uMcD";
-      description = ''
-        The apikey to authorise the OPNSense appliance access.
-        - This will set the required "OPN_APIKEY" enviroment variable. 
-        - Details: https://github.com/paepckehh/opnborg 
-        '';
-    };
-    
-    apisecret = mkOption {
-      type = types.str;
-      default = null;
-      example = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
-      description = ''
-        The apisecret to authorise the OPNSense appliance access.
-        - This will set the required "OPN_APISECRET" enviroment variable. 
-        - Details: https://github.com/paepckehh/opnborg
-        '';
-    };
-    
-    tlskeypin = mkOption {
-      type = types.str;
-      default = null;
-      example = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
-      description = ''
-        The tlskeypin of the OPNSense appliance WebUI (selfsigned-) certificate."
-        - This will set the optional "OPN_TLSKEYPIN" enviroment variable.
-        - Details: https://github.com/paepckehh/opnborg
-        '';
-    };
-    
-    targets = mkOption {
-      type = types.str;
-      default = null;
-      example = "opn001.admin.lan:443,opn002.admin.lan:443,opn002.admin.lan:443,opn004.admin.lan:443";
-      description = ''
-         The OPNSense appliance(s) target hostname(s)[opt:port]. 
-        - This will set the "OPN_TARGETS" enviroment variable.
-        - This string expects a comma seperated list.
-        - Either OPN_TARGETS or freeform OPN_TARGETS_[...] extraOptions enironment variables are required. 
-        - Details: https://github.com/paepckehh/opnborg
-        '';
-    };
     
     user = mkOption {
       type = types.str;
@@ -68,33 +22,32 @@ in {
       default = {};
       example = ''
         # minimal
+        "OPN_TARGETS" = "opn01.lan";
         "OPN_APIKEY" = "+RIb6YWNdcDWMMM7W5ZYDkUvP4qx6e1r7e/Lg/Uh3aBH+veuWfKc7UvEELH/lajWtNxkOaOPjWR8uMcD";
         "OPN_APISECRET" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
-        "OPN_TARGETS" = "opn00.lan";
         # complex
         "OPN_APIKEY" = "+RIb6YWNdcDWMMM7W5ZYDkUvP4qx6e1r7e/Lg/Uh3aBH+veuWfKc7UvEELH/lajWtNxkOaOPjWR8uMcD"
         "OPN_APISECRET" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p"
         "OPN_TLSKEYPIN" = "8VbjM3HKKqQW2ozOe5PTicMXOBVi9jZTSPCGfGrHp8rW6m+TeTxHyZyAI1GjERbuzjmz6jK/usMCWR/p";
-        "OPN_MASTER" = "opn00.lan:8443"
-        "OPN_TARGETS_HOTSTANDBY" = "opn00.lan:8443"
-        "OPN_TARGETS_IMGURL_HOTSTANDBY" = "https://avatars.githubusercontent.com/u/120342602?s=96&v=4"
-        "OPN_TARGETS_PRODUCTION='opn01.lan:8443,opn02.lan:8443"
-        "OPN_TARGETS_IMGURL_HOTSTANDBY" = "https://avatars.githubusercontent.com/u/120342602?s=96&v=4"
-        "OPN_SLEEP" = "60"
-        "OPN_DEBUG" = "true"
-        "OPN_SYNC_PKG" ="true"
-        "OPN_HTTPD_ENABLE" = "true"
-        "OPN_HTTPD_SERVER" = "127.0.0.1:6464"
-        "OPN_HTTPD_COLOR_FG" = "white"
-        "OPN_HTTPD_COLOR_BG" = "grey"
-        "OPN_RSYSLOG_ENABLE='true'
-        "OPN_RSYSLOG_SERVER='192.168.122.1:5140'
-        "OPN_GRAFANA_WEBUI='http://localhost:9090'
-        "OPN_GRAFANA_DASHBOARD_FREEBSD='Kczn-jPZz/node-exporter-freebsd'
-        "OPN_GRAFANA_DASHBOARD_HAPROXY='rEqu1u5ue/haproxy-2-full'
-        "OPN_WAZUH_WEBUI='http://localhost:9292'
-        "OPN_PROMETHEUS_WEBUI='http://localhost:9191'
-        }
+        "OPN_MASTER" = "opn00.lan:8443";
+        "OPN_TARGETS_HOTSTANDBY" = "opn00.lan:8443";
+        "OPN_TARGETS_PRODUCTION" = "opn01.lan:8443,opn02.lan:8443";
+        "OPN_TARGETS_IMGURL_HOTSTANDBY" = "https://icon-library.com/images/freebsd-icon/freebsd-icon-16.jpg";
+        "OPN_TARGETS_IMGURL_PRODUCTION" = "https://icon-library.com/images/freebsd-icon/freebsd-icon-16.jpg";
+        "OPN_SLEEP" = "60";
+        "OPN_DEBUG" = "true";
+        "OPN_SYNC_PKG" = "true";
+        "OPN_HTTPD_ENABLE" = "true";
+        "OPN_HTTPD_SERVER" = "127.0.0.1:6464";
+        "OPN_HTTPD_COLOR_FG" = "white";
+        "OPN_HTTPD_COLOR_BG" = "grey";
+        "OPN_RSYSLOG_ENABLE" = "true";
+        "OPN_RSYSLOG_SERVER" = "192.168.122.1:5140";
+        "OPN_GRAFANA_WEBUI" = "http://localhost:9090";
+        "OPN_GRAFANA_DASHBOARD_FREEBSD" = "Kczn-jPZz/node-exporter-freebsd";
+        "OPN_GRAFANA_DASHBOARD_HAPROXY" = "rEqu1u5ue/haproxy-2-full";
+        "OPN_WAZUH_WEBUI" = "http://localhost:9292";
+        "OPN_PROMETHEUS_WEBUI" = "http://localhost:9191";
         '';
       description = ''
         Additional setup enviroment variables
